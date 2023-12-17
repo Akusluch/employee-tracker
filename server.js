43e1270a5ad1898b9a83e41ddb29cdea42c46f84
Alphonse Kusluch <alkusluch@gmail.com>
2023-12-17 12:19:34 -0500

    add seeds.sql and a start for the inquier prompt

--- server.js
+++ server.js
@@ -10,5 +10,41 @@ const db = mysql.createConnection(
       password: 'Bingo$16141',
       database: 'tech_db'
     },
-    console.log(`Connected to the tech_db database.`)
-  );
+    console.log(`Connected to the tech_db database.`),
+    employeeTracker();
+  )
+
+  var employeeTracker = function () {
+    inquirer.prompt([{
+      type: 'list',
+      name: 'prompt',
+      message: 'What would you like to do?',
+      choices: ['View ALL Department', 'View ALL Roles', 'View ALL Employees']
+    }]).then((answers) => {
+      // handle department view
+      if (answers.prompt === 'View ALL Department') {
+        db.query('select * from department', (err, result) => {
+          if (err) throw err;
+          console.log('viewing ALL departments');
+          console.table(result);
+          employeeTracker();
+        });
+        // handle role view
+      } else if (answers.prompt === 'View ALL Roles') {
+        db.query('select * from role', (err, result) => {
+          if (err) throw err;
+          console.log("Viewing All Roles: ");
+          console.table(result);
+          employeeTracker(); 
+        });
+        // handle employee view
+      } else if (answers.prompt === 'View ALL Employees') {
+        db.query('select * from employee', (err, result) => {
+          if (err) throw err;
+          console.log("Viewing All Employees: ");
+          console.table(result);
+          employeeTracker(); 
+        });
+      }
+    })
+  }
