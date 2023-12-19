e528ad6638a2cb8040fec4bf8f7c30a2994b40e1
Alphonse Kusluch <alkusluch@gmail.com>
2023-12-18 20:57:08 -0500

    added the add department and log out option to the inquier prompt

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const db = mysql.createConnection(
       database: 'tech_db'
      })
 
-
 db.connect(err => {
   if (err) throw err;
 
@@ -19,39 +18,65 @@ db.connect(err => {
   employeeTracker();
 })
 
-
-
-  var employeeTracker = function () {
-    inquirer.prompt([{
-      type: 'list',
-      name: 'prompt',
-      message: 'What would you like to do?',
-      choices: ['View ALL Department', 'View ALL Roles', 'View ALL Employees']
-    }]).then((answers) => {
-      // handle department view
-      if (answers.prompt === 'View ALL Department') {
-        db.query('select * from department', (err, result) => {
-          if (err) throw err;
-          console.log('viewing ALL departments');
-          console.table(result);
-          employeeTracker();
-        });
-        // handle role view
-      } else if (answers.prompt === 'View ALL Roles') {
-        db.query('select * from role', (err, result) => {
-          if (err) throw err;
-          console.log("Viewing All Roles: ");
-          console.table(result);
-          employeeTracker(); 
-        });
-        // handle employee view
-      } else if (answers.prompt === 'View ALL Employees') {
-        db.query('select * from employee', (err, result) => {
-          if (err) throw err;
-          console.log("Viewing All Employees: ");
-          console.table(result);
-          employeeTracker(); 
-        });
-      }
-    })
-  }
+var employeeTracker = function () {
+  //initail quetion
+  inquirer.prompt([{
+    type: 'list',
+    name: 'prompt',
+    message: 'What would you like to do?',
+    choices: ['View ALL Department', 'View ALL Roles', 'View ALL Employees','Add A Department', 'Log Out']
+  }]).then((answers) => {
+    // handle view all departments
+    if (answers.prompt === 'View ALL Department') {
+      db.query('select * from department', (err, result) => {
+        if (err) throw err;
+        console.log('viewing ALL departments');
+        console.table(result);
+        employeeTracker();
+      });
+      // handle view all roles
+    } else if (answers.prompt === 'View ALL Roles') {
+      db.query('select * from role', (err, result) => {
+        if (err) throw err;
+        console.log("Viewing All Roles: ");
+        console.table(result);
+        employeeTracker(); 
+      });
+      // handle view all employees
+    } else if (answers.prompt === 'View ALL Employees') {
+      db.query('select * from employee', (err, result) => {
+        if (err) throw err;
+        console.log("Viewing All Employees: ");
+        console.table(result);
+        employeeTracker(); 
+      });
+      // haddle adding Department
+    } else if (answers.prompt === 'Add A Department') {
+      // prompt for name of new department
+      inquirer.prompt([{
+          type: 'input',
+          name: 'department',
+          message: 'What is the name of the dpeartment?',
+          // confirm user input
+          validate: departmentInput => {
+              if (departmentInput) {
+                  return true;
+              } else {
+                  console.log('Please Add A Department!');
+                  return false;
+              }
+          }
+      }]).then((answers) => {
+          db.query(`INSERT INTO department (name) VALUES (?)`, [answers.department], (err, result) => {
+              if (err) throw err;
+              console.log(`Added ${answers.department} to the database.`)
+              employeeTracker();
+          });
+      })
+      // handle log out
+    } else if (answers.prompt === 'Log Out') {
+      db.end();
+      console.log('Bye')
+    }
+  })
+}
